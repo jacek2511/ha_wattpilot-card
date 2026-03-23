3fb940dec67e58117c854b3510d189cf53cf7854
jacek2511 <112733566+jacek2511@users.noreply.github.com>
2026-03-23 23:43:41 +0100

    Update wattpilot-card.ts

--- src/wattpilot-card.ts
+++ src/wattpilot-card.ts
@@ -283,8 +283,8 @@ export class WattpilotCard extends LitElement {
             <span>Phases</span>
             <div class="chips">
               <div class="chip ${phases === 'Auto' ? 'active' : ''}" @click=${() => this._setPhases('Auto')}>Auto</div>
-              <div class="chip ${phases === '1' ? 'active' : ''}" @click=${() => this._setPhases('1')}>1</div>
-              <div class="chip ${phases === '3' ? 'active' : ''}" @click=${() => this._setPhases('3')}>3</div>
+              <div class="chip ${phases === '1' ? 'active' : ''}" @click=${() => this._setPhases('1 Phase')}>1</div>
+              <div class="chip ${phases === '3' ? 'active' : ''}" @click=${() => this._setPhases('3 Phases')}>3</div>
             </div>
           </div>
           
